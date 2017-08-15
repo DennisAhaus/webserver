ca03abc428b237c3eee80b93e677e6afb41758fa
Dennis Ahaus <dennis.ahaus@sap.com>
2017-08-15 09:49:47 +0200

    Create a http webserver with https test certificates

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const config = require('config');
 const cluster = require('cluster');
-const port = config.get('server.port');
+const fs = require('fs');
 const numCPUs = require('os').cpus().length;
+const https = require('https')
+const http = require('http')
 
 const app = require('./lib/app');
+const httpPort = config.get('server.http.port');
+
 
 if (cluster.isMaster) {
     console.log(`Master ${process.pid} is running`);
@@ -11,17 +15,43 @@ if (cluster.isMaster) {
     for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
     }
-
+    
     cluster.on('exit', (worker, code, signal) => {
         console.log(`Worker ${worker.process.pid} died`);
     });
-
+    
 } else {
+    
+    console.log(`Starting Worker ${process.pid}...`);
+    config.get('server.http.port');
 
-    app.listen(port, () => {
-        console.log(`Worker ${process.pid} listen on port ${port}`);
-    });
+    if (config.get('server.https') != null) {
 
-    console.log(`Worker ${process.pid} started`);
+        const options = {
+            cert: fs.readFileSync(config.get('server.https.options.cert')),
+            key: fs.readFileSync(config.get('server.https.options.key')),
+        };
+
+        if (config.has('server.https.options.passphrase')) {
+            options.passphrase = config.get('server.https.options.passphrase');
+        }
+
+        if (config.has('server.http.httpRedirect')) {
+            // set up a route to redirect http to https
+            const redirectTo = config.get('server.http.httpRedirect');
+            console.log(`HTTP server with redirection to '${redirectTo}' is started`);
+
+            http.createServer((req, res) => {
+                res.writeHead(302, { 'Location': redirectTo + req.url });
+                res.end();
+            }).listen(httpPort);
+        }
+
+        module.exports = https.createServer(options, app).listen(config.get('server.https.port'));
+
+    } else {
+        module.exports = http.createServer(app).listen(httpPort);
+    }
+
 }
 
---
+++ test/scenario/test-http-server.js
@@ -0,0 +1,29 @@
+const supertest = require('supertest');
+const app = require('../../index');
+const fs = require('fs');
+const htmlData1 = '' + fs.readFileSync(__dirname + '/../resources/public1/index.html');
+const htmlData2 = '' + fs.readFileSync(__dirname + '/../resources/public2/index2.html');
+
+const client = supertest(app);
+
+describe(__filename, () => {
+    describe('Test server', () => {
+        describe('GET /', () => {
+
+            it('should get website html', done => {
+                client
+                    .get('/')
+                    .expect(200, htmlData1)
+                    .end(done);
+            });
+
+            it('should get seconds static website html', done => {
+                client
+                    .get('/index2.html')
+                    .expect(200, htmlData2)
+                    .end(done);
+            });
+
+        });
+    });
+});
